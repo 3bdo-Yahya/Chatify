fbab629a48e7186af18eacbaa55cba6b20ca3cbc
3bdo-yahya <A.Rhman.Yahya@gmail.com>
2025-10-08 01:59:26 +0300

    Add WebSocket functionality and related dependencies

---
+++ backend/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+    conn, err := websocket.Upgrade(w, r)
+    if err != nil {
+		http.Error(w, "websocket upgrade failed", http.StatusBadRequest)
+        fmt.Println("upgrade error:", err)
+        return
+    }
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+    pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Chat App v0.1")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
